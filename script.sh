9395cb030a3c029d635ebbb2001ed4e1dbad6303
abouabra <aymanbouabra@gmail.com>
2023-01-15 15:53:16 +0100

    1337

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 cd && cd goinfre && mkdir docker && rm -rf ~/Library/Containers/com.docker.docker && ln -s ~/goinfre/docker ~/Library/Containers/com.docker.docker
 /usr/local/munki/launchapp -a "Docker"
-curl curl https://raw.githubusercontent.com/abouabra/valgrind_1337/master/Dockerfile -o Dockerfile
+curl https://raw.githubusercontent.com/abouabra/valgrind_1337/master/Dockerfile -o Dockerfile
 docker build -t valgrind_1337 .
 echo "alias valgrind='docker run -it --rm -v $1:/valgrind valgrind_1337'" >> ~/.zshrc
 echo "alias valgrind='docker run -it --rm -v $1:/valgrind valgrind_1337'" >> ~/.bashrc
